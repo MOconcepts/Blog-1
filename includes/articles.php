91961b9a07c6808a4eccc22d35f565f3c3ee425d
Dan Harris <danharris1606@gmail.com>
2016-01-09 16:09:09 +0000

    Restricted amount of articles retrieved at any one time

--- includes/articles.php
+++ includes/articles.php
@@ -1,53 +1,55 @@
 <?php
 	class articles {
-		private $all; //Used to store all articles
 		private $aConn; //Used for database connection for queries
-		
+		private $start; //Stores starting article number
+		private $finish; //Stores finishing article number
+
 		//Constructor to store connection as field, then gets articles
-		function __construct($conn){ 
+		function __construct($conn){
 			$this->aConn = $conn;
-			$this->getArticlesFromDb();
 		}
 
 		//Runs a query to get all articles in table and puts results into field object
-		public function getArticlesFromDb(){
-			$query = "SELECT * FROM Posts ORDER BY `ID` DESC";
-			$this->all = $this->aConn->query($query);
+		public function getArticlesFromDb($page){
+			$this->start = (($page-1)*5); //Calculates start and finish values based on page number
+			$this->finish = $this->getFinish($start,5);
+			$query = "SELECT * FROM Posts ORDER BY `ID` DESC LIMIT $this->start,5"; //Limits results to 5 based on start number
+			return $this->aConn->query($query); //Returns all articles
 		}
-		
-		//Will go to a specific point in article list (starts from 0) and will return a field in that record
-		public function getArticle($id,$component){
-			$this->all->data_seek($id);
-			$row = $this->all->fetch_assoc();
-			return $row[$component]; 
+
+		public function getArticleById($id){
+			$query = "SELECT * FROM Posts WHERE `ID` = $id";
+			return $this->aConn->query($query)->fetch_assoc();
 		}
-		
+
 		//Returns total number of articles
 		public function getTotal(){
-			return $this->all->num_rows;
+			$result = $this->aConn->query("SELECT COUNT(*) AS total FROM Posts"); //Runs SQL Count Function
+			$total = $result->fetch_assoc();
+			return $total['total']; //Returns total number of articles
 		}
-		
+
 		//Calculates a number for when printing articles by ensuring it does not exceed total number
 		public function getFinish($start,$count){
 			if ($start+$count>$this->getTotal()){
-				return $this->getTotal();				
+				return $this->getTotal();
 			} else {
 				return $start +$count;
 			}
 		}
-		
+
 		//Checks to see whether a page with newer articles is present
-		public function newVisible($start){
-			if ($start <= 0){
+		public function newVisible(){
+			if ($this->start <= 0){
 				return false;
 			} else {
 				return true;
 			}
 		}
-		
+
 		//Checks to see whether a page with older articles is present
-		public function oldVisible($start,$count){				
-			if ($start+$count>$this->getTotal()-1){ //is -1 here as $start begins at 0 whereas the total will be starting from 1
+		public function oldVisible(){
+			if ($this->start+5>$this->getTotal()-1){ //is -1 here as $start begins at 0 whereas the total will be starting from 1
 				return false;
 			} else {
 				return true;
--- index.php
+++ index.php
@@ -6,29 +6,30 @@
 	include "includes/articles.php";
 	session_start();
 
-	$conn = new mysqli("localhost",DBUSERNAME,DBPASSWORD,DB);
+	$conn = new mysqli("localhost",DBUSERNAME,DBPASSWORD,DB); //Esatblish database connection 
 	$articles = new articles($conn); //Initialised object
+
 	$p = $_GET["p"]; //gets variable for page
 	if ($p ==""){
-		$p = 1;
+		$p = 1; //If no page specified assume default value 1
 	}
-	$start = ($p-1)*5; //Converts page number into starting point
-
-	for ($i = $start;$i<$articles->getFinish($start,5);$i++){
-		echo "<h1>".$articles->getArticle($i, "Title")."</h1>"; //Title of article
-		echo "<h3>By: ".$articles->getArticle($i, "Author")."</h3>"; //Author of article
-		echo "<p>".$articles->getArticle($i, "Content")."</p>"; //Main content of article
-		echo "<P>".$articles->getArticle($i, "Date")."</p>"; //Date article was created
+	$articlesToShow = $articles->getArticlesFromDb($p); //Retrieves articles for page number
+	for ($i = 0;$i<$articlesToShow->num_rows;$i++){ //Runs through the 5 articles
+		$articlesToShow->data_seek($i);
+		$article = $articlesToShow->fetch_assoc(); //Fetches relavent article
+		echo "<h1>".$article["Title"]."</h1>"; //Title of article
+		echo "<h3>By: ".$article["Author"]."</h3>"; //Author of article
+		echo "<p>".$article["Content"]."</p>"; //Main content of article
+		echo "<P>".$article["Date"]."</p>"; //Date article was created
 	}
-
-
-	if ($articles->newVisible($start)){ //Displays buttons depending on whether article methods return true
+	if ($articles->newVisible()){ //Displays buttons depending on whether article methods return true
 			echo "<a href='index.php?p=".($p-1) ."'>Previous Page</a>";
 		}
 
-		if ($articles->oldVisible($start,5)){
+		if ($articles->oldVisible()){
 			echo '<a href="index.php?p='.($p+1) .'">Next Page</a>';
 		}
+
 ?>
 </body>
 </html>
