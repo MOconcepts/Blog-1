a545be41432f73a3e4003a8d4a6cc875e964aef1
Dan Harris <danharris1606@gmail.com>
2016-01-09 15:09:37 +0000

    Fixed navigation links, added connection constructor and added default page value

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
 	include "includes/articles.php";
 	session_start();
 
-	
+	$conn = new mysqli("localhost",DBUSERNAME,DBPASSWORD,DB);
 	$articles = new articles($conn); //Initialised object
 	$p = $_GET["p"]; //gets variable for page
+	if ($p ==""){
+		$p = 1;
+	}
 	$start = ($p-1)*5; //Converts page number into starting point
-	
+
 	for ($i = $start;$i<$articles->getFinish($start,5);$i++){
 		echo "<h1>".$articles->getArticle($i, "Title")."</h1>"; //Title of article
 		echo "<h3>By: ".$articles->getArticle($i, "Author")."</h3>"; //Author of article
@@ -18,13 +21,13 @@
 		echo "<P>".$articles->getArticle($i, "Date")."</p>"; //Date article was created
 	}
 
-	
+
 	if ($articles->newVisible($start)){ //Displays buttons depending on whether article methods return true
-			echo "<a href='newIndex.php?p=".($p-1) ."'>Previous Page</a>";
+			echo "<a href='index.php?p=".($p-1) ."'>Previous Page</a>";
 		}
-		
+
 		if ($articles->oldVisible($start,5)){
-			echo '<a href="newIndex.php?p='.($p+1) .'">Next Page</a>';
+			echo '<a href="index.php?p='.($p+1) .'">Next Page</a>';
 		}
 ?>
 </body>
